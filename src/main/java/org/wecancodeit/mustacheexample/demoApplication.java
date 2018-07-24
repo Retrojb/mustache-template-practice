c0732cd844e686d0c956022e811e959752a30ef3
Retrojb <baltes.26@osu.edu>
2018-07-24 10:44:51 -0400

    added web security config adapter depenecy

---
+++ src/main/java/org/wecancodeit/mustacheexample/demoApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.mustacheexample;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+public class demoApplication extends WebSecurityConfigurerAdapter{
+	
+	
+	@Override
+
+}
