f64043a0b7d2eb0828a10c5c778f9f8ab13346e7
Retrojb <baltes.26@osu.edu>
2018-07-24 10:26:18 -0400

    inital build

---
+++ src/main/java/org/wecancodeit/mustacheexample/MustacheExampleApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.mustacheexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustacheExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MustacheExampleApplication.class, args);
+	}
+}
